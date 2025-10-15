2763f6c9e7ba656c0b24b32d1b77fc18424ac1cb
Aditya Lohuni <adityalohuni@gmail.com>
2025-10-15 11:29:00 +0200

    Add business logic services for all modules

---
+++ backend/src/services/mod.rs
@@ -0,0 +1,12 @@
+pub mod auth_service;
+pub mod vehicle_service;
+pub mod driver_service;
+pub mod assignment_service;
+pub mod maintenance_service;
+pub mod logistics_service;
+pub mod telemetry_service;
+pub mod financial_service;
+pub mod settings_service;
+pub mod user_service;
+
+
